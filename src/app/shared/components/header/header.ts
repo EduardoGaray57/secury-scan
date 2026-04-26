27f9f0f3fb138ccfb4f9103aed27d1b16b987335
EduardoGaray57 <eduardomarcelo03@gmail.com>
2026-04-25 21:08:45 -0400

    feat: add dark mode toggle and password strength indicator

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,11 +1,28 @@
 import { Component } from "@angular/core";
-import { MatToolbarModule } from '@angular/material/toolbar'
-import { MatIconModule } from '@angular/material/icon'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule],
-  templateUrl: './header.html'
+  imports: [
+    MatToolbarModule, 
+    MatIconModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    CommonModule
+  ],
+  templateUrl: './header.html',
+  styleUrl: './header.css'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isDarkMode = false;
+
+  toggleTheme(): void {
+    this.isDarkMode = !this.isDarkMode;
+    document.body.classList.toggle('dark-theme', this.isDarkMode)
+  }
+}
